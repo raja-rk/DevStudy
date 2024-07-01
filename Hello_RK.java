09fa26311fc3dc4574a4efed08dc77b5c6f3f0ac
Raj Kumar <rk9337979@gmail.com>
2024-07-01 13:48:59 +0530

    Data types, Operators, conditional Statements, Loops

--- Hello_RK.java
+++ Hello_RK.java
@@ -1,5 +1,11 @@
 class Hello{
     public static void main(String a[]){
+        int a1 = 1;
+        float b = 2.0f;
+        long l = 124l;
+        double d = 5.8;
+        char c = 'k';
+        boolean b1 = true;
         System.out.print("Hello World");
     }
 }
